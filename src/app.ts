871d50512338849a1aee0e88db1ae8b6083a25e9
wjchen127 <wjchen127@gmail.com>
2022-10-05 03:03:07 +0800

    增加頭貼與音檔的路由

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,13 @@ const port = 8080;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+app.get('/rss/someoneid', (req, res)=>{
+  res.setHeader('content-type', 'application/rss+xml')
+  // res.send('xml')
+})
+app.use('/getavatar', express.static(__dirname + '/public/avatar.jpg'))
+
+app.use('/getmp3', express.static(__dirname + '/public/test.mp3'))
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
