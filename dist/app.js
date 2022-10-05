8b7ac977ce88ee9afb091b353057ade39c45bef0
wjchen127 <wjchen127@gmail.com>
2022-10-05 11:49:52 +0800

    完成後端路由以及修改前端模板

--- dist/app.js
+++ dist/app.js
@@ -6,12 +6,66 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 8080;
+const bodyParser = require('body-parser');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('hello');
 });
 app.use('/getxml', express_1.default.static(__dirname + '/public/me.xml'));
 app.use('/getavatar', express_1.default.static(__dirname + '/public/avatar.jpg'));
 app.use('/getmp3', express_1.default.static(__dirname + '/public/test.mp3'));
+app.use('/someoneid/:id', (req, res) => {
+    res.send('這是某人的某集podcast頁面, 單集id為:' + req.params.id);
+});
+app.use('/upload1', (req, res) => {
+    // const feed = new Podcast({
+    //   title: '陳威捷的Podcast',
+    //   description: '展示用途',
+    //   feedUrl: 'https://podcast-feed-example-364516.de.r.appspot.com/getdexml',
+    //   siteUrl: 'https://podcast-feed-example-364516.de.r.appspot.com',
+    //   imageUrl: 'https://podcast-feed-example-364516.de.r.appspot.com/getavater',
+    //   author: '陳威捷',
+    //   copyright: '2022 陳威捷',
+    //   language: 'zh',
+    //   categories: ['Comedy'],
+    //   pubDate: 'Oct 5, 2022 04:00:00 GMT',
+    //   itunesAuthor: '陳威捷',
+    //   itunesSubtitle: 'I am a sub title',
+    //   itunesSummary: 'I am a summary',
+    //   itunesOwner: { name: '陳威捷', email: 'wjchen127@gmail.com' },
+    //   itunesExplicit: false,
+    //   itunesCategory: [{
+    //       text: 'Comedy'
+    //   }],
+    //   itunesImage: 'https://podcast-feed-example-364516.de.r.appspot.com/getavater'
+    // })
+    // feed.addItem({
+    //   title:  'EP1-單集標題',
+    //   description: 'EP1-的內容',
+    //   url: 'https://podcast-feed-example-364516.de.r.appspot.com/someoneid/fwfvwef', // link to the item
+    //   guid: 'fwfvwef', // optional - defaults to url
+    //   author: '陳威捷', // optional - defaults to feed author property
+    //   date: 'Oct 5, 2022', // any format that js Date can parse.
+    //   enclosure : {url:'https://podcast-feed-example-364516.de.r.appspot.com/getmp3'}, // optional enclosure
+    //   itunesAuthor: '陳威捷',
+    //   itunesExplicit: false,
+    //   itunesSubtitle: 'I am a sub title',
+    //   itunesSummary: 'I am a summary',
+    //   itunesDuration: 0.033,
+    // })
+    // const xml = feed.buildXml()
+    // res.send(xml)
+});
+app.post('/upload', (req, res) => {
+    console.log(req.body);
+    res.send("good");
+});
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,141 @@
 import express from 'express';
+import { Podcast } from 'podcast'
+const fs = require('fs')
+const busboy = require('busboy')
+const shortuuid = require('short-uuid')
 const app = express();
 const port = 8080;
+const bodyParser = require('body-parser')
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// set the view engine to ejs
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('hello');
 });
-app.use('/getxml', express.static(__dirname + '/public/me.xml'))
-app.use('/getavatar', express.static(__dirname + '/public/avatar.jpg'))
 
-app.use('/getmp3', express.static(__dirname + '/public/test.mp3'))
+app.get('/someoneid/:id',(req, res)=>{
+  res.send(`這是某人的podcast頁面, xml連結:https://podcast-feed-example-364516.de.r.appspot.com/getxml/${req.params.id}`)
+})
+
+
+
+app.post('/upload',(req, res)=>{
+  const uuid = shortuuid.generate()
+  console.log(uuid)
+  const bb = busboy({ headers: req.headers })
+  let formData = new Map()
+  req.pipe(bb)
+
+  
+  bb.on('file', (name, file, info) => {
+   
+    let extension = info.filename.split('.').pop()
+    const saveTo = `dist/public/${uuid}.${extension}`;
+    file.pipe(fs.createWriteStream(saveTo))
+  })
+  bb.on('field', (name, val, info) => {
+    formData.set(name, val)
+  })
+  bb.on('close', () => {
+    const channelTitle = formData.get('channelTitle')
+    const channelDescription = formData.get('channelDescription')
+    const author = formData.get('author')
+    const email = formData.get('email')
+    const episodeTitle = formData.get('episodeTitle')
+    const episodeDescription = formData.get('episodeDescription')
+    const feed = new Podcast({
+      title: channelTitle,
+      description: channelDescription,
+      feedUrl: `https://podcast-feed-example-364516.de.r.appspot.com/getxml/${uuid}`,
+      siteUrl: `https://podcast-feed-example-364516.de.r.appspot.com/someoneid/${uuid}`,
+      imageUrl: `https://podcast-feed-example-364516.de.r.appspot.com/getavater/${uuid}`,
+      author: formData.get('author'),
+      copyright: `2022 ${author}`,
+      language: 'zh',
+      categories: ['Comedy'],
+      pubDate: new Date(),
+      itunesAuthor: author,
+      itunesSubtitle: 'I am a sub title',
+      itunesSummary: 'I am a summary',
+      itunesOwner: { name: author, email: email },
+      itunesExplicit: false,
+      itunesCategory: [{
+          text: 'Comedy'
+      }],
+      itunesImage: `https://podcast-feed-example-364516.de.r.appspot.com/getavater/${uuid}`
+    })
+    
+    feed.addItem({
+      title:  episodeTitle,
+      description: episodeDescription,
+      url: `https://podcast-feed-example-364516.de.r.appspot.com/someoneid//${uuid}`, // link to the item
+      guid: uuid, // optional - defaults to url
+      author: author, // optional - defaults to feed author property
+      date: new Date(), // any format that js Date can parse.
+      enclosure : {url:`https://podcast-feed-example-364516.de.r.appspot.com/getmp3/${uuid}`}, // optional enclosure
+      itunesAuthor: author,
+      itunesExplicit: false,
+      itunesSubtitle: 'I am a sub title',
+      itunesSummary: 'I am a summary',
+      itunesDuration: 0.033,
+    })
+
+    const xml = feed.buildXml()
+    fs.writeFileSync(`dist/public/${uuid}.xml`,xml)
+    res.json({id: `${uuid}`, redirect: true})
+  })
+})
+
+app.get('/getxml/:id',(req, res, next)=>{
+  try{
+    if (fs.existsSync(`dist/public/${req.params.id}.xml`)) {
+      const data = fs.readFileSync(`dist/public/${req.params.id}.xml`,{encoding:'utf8', flag:'r'})
+      res.set('Content-Type', 'text/xml')
+      res.send(data)
+    }else{
+      res.send("404 not found")
+    }
+  }catch(e){
+    next(e)
+  }
+})
+
+app.get('/getmp3/:id',(req, res, next)=>{
+  try{
+    if (fs.existsSync(`dist/public/${req.params.id}.mp3`)) {
+      const data = fs.readFileSync(`dist/public/${req.params.id}.mp3`,{flag:'r'})
+      res.set('Content-Type', 'audio/mpeg')
+      res.send(data)
+    }else{
+      res.send("404 not found")
+    }    
+  }catch(e){
+    next(e)
+  }
+})
+
+app.get('/getavatar/:id',(req, res, next)=>{
+  try{
+    if (fs.existsSync(`dist/public/${req.params.id}.jpg`)) {
+      const data = fs.readFileSync(`dist/public/${req.params.id}.jpg`,{flag:'r'})
+      res.set('Content-Type', 'image/jpeg')
+      res.send(data)
+    }else{
+      res.send("404 not found")
+    }
+  }catch(e){
+    next(e)
+  }
+})
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
